1b5900fe1ec0f39d9c5377aa38e8e19ea77bde5c
Liam Bradley <liamhbradley11@gmail.com>
2020-10-23 15:59:38 -0400

    files/routes working correctly

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,27 @@
-let mysql = require("mysql");
+// Set up MySQL connection.
+var mysql = require("mysql");
 
-let connection;
-
-if (process.env.JAWSDB_URL) 
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-else 
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'burgers_db'
-    });
-
-connection.connect((err) => {
-    if (err) {
-        console.error(`error connecting MySQL: ${err.stack} \n`);
-        return;
-    }
-    console.log(`Connected to MySQL as id ${connection.threadId} \n`);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
 });
 
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
 module.exports = connection;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese Burger', false);
