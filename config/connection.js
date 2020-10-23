edbc84b2d91487741bc00eb2137bd8c47f6fc01a
Liam Bradley <liamhbradley11@gmail.com>
2020-10-23 19:39:10 -0400

    JAWS

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 8080,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: "cf4pt95mct5ikjbd	",
+  password: "h5mc003a4lr7d29t",
+  database: "k3pzom8f0snocuhc"
 });
 
 }
